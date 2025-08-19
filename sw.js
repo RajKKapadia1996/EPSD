ac626adbeb9dd16931d6fa549fb7bde41cd1504d
RajKKapadia1996 <rajkkapadia1996@gmail.com>
2025-08-20 00:09:05 +0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("smashroom-store").then(cache => cache.addAll([
+      "/",
+      "/index.html",
+      "/vr.mp4",
+      "/ar.mp4",
+      "/icons/icon-192.png",
+      "/icons/icon-512.png"
+    ]))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
